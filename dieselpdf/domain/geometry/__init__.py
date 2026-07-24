dd76ba31c8f0d65060482c32c81ec887dba0bb78
hanhhhh2008-lang <hanhhhh2008@gmail.com>
2026-07-24 10:29:21 +1000

    Export Phase 2 policy refinements

--- dieselpdf/domain/geometry/__init__.py
+++ dieselpdf/domain/geometry/__init__.py
@@ -1,17 +1,34 @@
 from .calibration import (
+    CalibrationAssessment,
+    CalibrationDisposition,
     CalibrationRecord,
     CalibrationResidual,
     ControlPointPair,
     fit_calibration,
 )
+from .datum import ProjectCoordinatePolicy, ProjectOriginKind, SurveyDatumLink
 from .grids import GridIntersection, GridLine2D, GridSystem
 from .points import Point2D, Point3D, Vector2D
-from .snapping import SnapCandidate, SnapKind, SnapResult, SnappingService
-from .tolerances import SourceQuality, ToleranceProfile, default_tolerance_profiles
+from .snapping import (
+    MergeAssessment,
+    MergeDecision,
+    SnapCandidate,
+    SnapKind,
+    SnapResult,
+    SnappingService,
+)
+from .tolerances import (
+    SourceQuality,
+    ToleranceProfile,
+    default_tolerance_profiles,
+    raster_tolerance_profile,
+)
 from .transforms import AffineTransform2D, CoordinateSystem2D
 
 __all__ = [
     "AffineTransform2D",
+    "CalibrationAssessment",
+    "CalibrationDisposition",
     "CalibrationRecord",
     "CalibrationResidual",
     "ControlPointPair",
@@ -19,15 +36,21 @@ __all__ = [
     "GridIntersection",
     "GridLine2D",
     "GridSystem",
+    "MergeAssessment",
+    "MergeDecision",
     "Point2D",
     "Point3D",
+    "ProjectCoordinatePolicy",
+    "ProjectOriginKind",
     "SnapCandidate",
     "SnapKind",
     "SnapResult",
     "SnappingService",
     "SourceQuality",
+    "SurveyDatumLink",
     "ToleranceProfile",
     "Vector2D",
     "default_tolerance_profiles",
     "fit_calibration",
+    "raster_tolerance_profile",
 ]
